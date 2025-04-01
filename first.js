34b9c039d7162a3a501ff518f405a11dd7f9a5a8
27ssss <vinniksemen4@gmail.com>
2025-04-01 17:14:27 -0400

    comments

--- first.js
+++ first.js
@@ -1,3 +1,17 @@
+// 1 - use couples alg with casual for cycle
+// 2 - everytime we find a letter we increase counter by 1 
+// until iterator + 1 is not equal to our symbol
+// 3 - check the index where we stops and just save index from start point to end point of repeating words
+// 4 - create new string where we put all our decode
+// example:
+// bbbbccccddddaa - by steps
+// 1 - delete all b's and add index to substring
+// 2 - new final string is b4 and first string is  bbbbccccddddaa
+// 3 - delete all c's and add index to substring 
+// 4 - new final string is b4c4
+// 5 - new final string is b4c4d4a2
+
+
 var countAndSay = function(n) {
     return helper('1', n)
 };
