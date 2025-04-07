17e163bf010422cd30ddc808f29890e9fe783009
27ssss <vinniksemen4@gmail.com>
2025-04-07 14:37:21 -0400

    12

--- second.js
+++ second.js
@@ -4,25 +4,24 @@
 /// bottom -> top [bottom -> top] [right]
 
 var generateMatrix = function(n) {
-    let matrix = Array(n).fill().map(() => Array(n).fill(0));
-    let left = 0;
+    let matrix = Array.from({ length: n }, () => Array(n).fill(0));    let left = 0;
     let right = n - 1;
     let top = 0;
     let bottom = n - 1;
     let num = 1
     while (left <= right && top <= bottom){
-        for (i = left; i < right
+        for (i = left; i <= right
             ; i++){
             matrix[top][i] = num
             ++num
         }
         top += 1
-        for (i = top; i < bottom; i++){
+        for (i = top; i <= bottom; i++){
             matrix[i][right] = num
             ++num
         }
         right -= 1
-        if (top <= bottom){
+        if (left <= right){
         for (i = right; i >= left; i++){
             matrix[bottom][i] = num
             ++num
